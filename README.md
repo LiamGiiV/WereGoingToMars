71ec3616154c1dbac912d6d010d3ccb9ab33f283
LiamGiiV <43221209+LiamGiiV@users.noreply.github.com>
2022-07-12 10:50:58 -0400

    Project Set-up

---
+++ README.md
@@ -0,0 +1,3 @@
+#We're Going To Mars! - v0.0.0.1
+
+For the fine folks at Kinaxis.
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+package liamgiiv.weregoingtomars
+
+object Main {
+  def main(args: Array[String]) = {
+    println("Hello, world!")
+  }
+}
